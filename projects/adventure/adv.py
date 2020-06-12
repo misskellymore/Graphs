ca28e403b081ccaf0aa412a833a3f898e0c64474
Kelly Moreira <moreira.kd@outlook.com>
2020-06-12 13:52:41 -0400

    adv so far

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -5,6 +5,19 @@ from world import World
 import random
 from ast import literal_eval
 
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+
 # Load world
 world = World()
 
@@ -28,13 +41,41 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+traveral_graph = dict()
 
 
 
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
-visited_rooms.add(player.current_room)
+# visited_rooms.add(player.current_room)
+
+s = Stack()
+s.push()([player.current_room])
+
+while s.size() > 0:
+    path = s.pop()
+    cur = path[-1]
+
+    if cur not in visited_rooms:
+        visited_rooms.add(cur)
+        traversal_graph[cur.id] = {"n": "?", "s": "?", "e": "?", "w": "?", }
+
+        for ex in player.current_room.get_exits():
+
+            if cur.get_room_in_direction(ex) != None:
+                traversal_path.append(ex)
+                traversal_graph[cur.id][ex] = cur.get_room_in_direction(ex).id
+                nxt = cur.get_room_in_direction(ex)
+                new_path = [*path, nxt]
+                s.push(new_path)
+
+            else:
+                traversal_graph[cur.id][ex] = None
+
+
+print("traversal", traversal_path)
+print("traversal graph", traversal_graph)
 
 for move in traversal_path:
     player.travel(move)
